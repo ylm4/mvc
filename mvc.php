57958e1e7c8033630a751c6e8289bcdc16a91451
Yanira Morales <ylm4@njit.edu>
2016-12-19 08:44:08 -0500

    mvc

---
+++ mvc.php
@@ -0,0 +1,40 @@
+<?php
+//creating program so user can reset password if needed.
+
+class Model {
+	public $word;
+	public function __construct(){
+		$this->word="Hello";
+	}
+}
+
+class View  {
+	private $model;
+	private $controller;
+	public function __construct($controller,$model) {
+		$this->controller=$controller;
+		$this->model=$model;
+	}
+	public function output(){
+		return '<p> <a href="mvc.php?action=clicked"' . $this->model->word . '</></p>';
+	//return '<p><a  href = "mvc.php?action=clicked"' . $this->model->word . "</a></p>";
+	}
+}
+
+class Controller { 
+	private $model;
+	public function __construct($model){
+		$this->model = $model;
+	}
+
+}
+echo '<h3>MVC Application</h3>';
+echo '<h4>View Github <a href= " ">Here</a></h4>';
+$model = new Model();
+$controller= new Controller($model);
+$view = new View ($controller, $model);
+
+
+echo $view->outtput();
+
+?>
